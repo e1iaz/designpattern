37ecfcc3ca40ccd53f826fa33e7d214f6bd6227c
ncabhd <ncabhd@hotmail.com>
2019-03-21 08:59:35 +0800

    学习《图解设计模式》

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import Iterator.Book;
+import Iterator.BookShelf;
+import Iterator.Iterator;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        BookShelf bookShelf=new BookShelf(4);
+        bookShelf.appendBook(new Book("Around"));
+        bookShelf.appendBook(new Book("Bible"));
+        bookShelf.appendBook(new Book("Cinderella"));
+        bookShelf.appendBook(new Book("Daddy"));
+        Iterator iterator=bookShelf.iterator();
+        while(iterator.hasNext()){
+            Book book=(Book)iterator.next();
+            System.out.println(book.getName());
+        }
+
+    }
+
+}
