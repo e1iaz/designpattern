e755c96ff65bcfbadacc69f57ffe21fb74468cf3
ncabhd <ncabhd@hotmail.com>
2019-09-12 17:11:14 +0800

    添加了原型模式

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import factorymethod.factory.Animal;
 import factorymethod.factory.AnimalFactory;
 import factorymethod.factory.DogFactory;
+import prototype.MessageBox;
+import prototype.UnderlinePen;
+import prototype.framework.Manager;
+import prototype.framework.Product;
 import proxy.Printable;
 import proxy.PrinterProxy;
 
@@ -127,9 +131,25 @@ public class Main {
 //        System.out.println("现在的名字是" + p.getPrinterName() + "。");
 //        p.print("Hello, world.");
 
-        AnimalFactory f = new DogFactory();
-        Animal a = f.createAnimal();
-        a.eat();
+//        factory
+//        AnimalFactory f = new DogFactory();
+//        Animal a = f.createAnimal();
+//        a.eat();
+
+        Manager manager = new Manager();
+        UnderlinePen upen = new UnderlinePen('~');
+        MessageBox mBox = new MessageBox('*');
+        MessageBox sBox = new MessageBox('/');
+        manager.register("strong message", upen);
+        manager.register("warning box", mBox);
+        manager.register("slash box", sBox);
+
+        Product p1 = manager.create("strong message");
+        p1.use("Hello, world.");
+        Product p2 = manager.create("warning box");
+        p2.use("Hello, world.");
+        Product p3 = manager.create("slash box");
+        p3.use("Hello, world.");
 
     }
 
